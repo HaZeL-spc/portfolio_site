ce5e2e749425c73848905e76d80e48d79195dadb
Kacper <kacper.8leszczynski8@gmail.com>
2022-02-20 23:51:23 +0100

    add source code

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-
   const copyText = (e) => {
-    const textElement = e.target.parentElement.parentElement.nextSibling.children[1]
-      
-    e.target.previousSibling.style.width = "100%"
-    e.target.classList.add("clicked")
+    const textElement =
+      e.target.parentElement.parentElement.nextSibling.children[1];
+
+    e.target.previousSibling.style.width = "100%";
+    e.target.classList.add("clicked");
 
     var selection = window.getSelection();
     var range = document.createRange();
@@ -14,7 +14,7 @@ const Contact = () => {
     selection.removeAllRanges();
     selection.addRange(range);
     document.execCommand("Copy");
-  }
+  };
 
   return (
     <div id="contact-site" className="contact-site">
@@ -24,31 +24,30 @@ const Contact = () => {
             <div className="w-100 d-flex justify-content-center mt-2">
               <div className="button-div" onClick={copyText}>
                 <div className="upper-line"></div>
-                <button 
-                  type="button" 
-                  className="mx-auto contact-copy-btn "
-                >Copy</button>
+                <button type="button" className="mx-auto contact-copy-btn ">
+                  Copy
+                </button>
               </div>
             </div>
             <div>
               <p className="font-sans text-center contact-text">Phone Number</p>
               <p className="font-sans text-center contact-text">796 213 412</p>
             </div>
-
           </div>
           <div className="col-12 col-lg-6 display-flex flex-column border-lg-left border-white border-top border-lg-top-0">
             <div className="w-100 d-flex justify-content-center mt-2">
               <div className="button-div" onClick={copyText}>
                 <div className="upper-line"></div>
-                <button 
-                  type="button" 
-                  className="mx-auto contact-copy-btn "
-                >Copy</button>
+                <button type="button" className="mx-auto contact-copy-btn ">
+                  Copy
+                </button>
               </div>
             </div>
             <div>
               <p className="font-sans text-center contact-text">E-mail</p>
-              <p className="font-sans text-center contact-text">kacper.leszczynski@gmail.com</p>
+              <p className="font-sans text-center contact-text">
+                kacper.leszczynski@gmail.com
+              </p>
             </div>
           </div>
         </div>
